d9b9d73a5af386e055c6a2e6aa04230ebf410e61
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2022-12-06 16:02:38 -0500

    Event emitter added

--- gaming-franchises-frontend/src/app/components/add-game/add-game.component.ts
+++ gaming-franchises-frontend/src/app/components/add-game/add-game.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Game } from 'src/app/helper/game-interface';
+import { GameServiceService } from 'src/app/services/game-service.service';
+import { GameDialogBoxComponent } from '../game-dialog-box/game-dialog-box.component';
 
 @Component({
   selector: 'app-add-game',
@@ -7,12 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddGameComponent implements OnInit {
 
-  constructor() { }
+  // Event Emitter to send game data
+  @Output() newGameEvent: EventEmitter<Game> = new EventEmitter<Game>();
+
+  // Define Game object
+  newGame: Game = {
+    title: '', description: '', creator: '', imgURL: '', type: '' 
+  };
+  tempId: string = '';
+  tempTags: string = '';
+
+  @Input() id: string = '';
+
+  constructor(private gameService: GameServiceService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
   openGameDialog() {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = false;
+    dialogConfig.width = "300px";
+    dialogConfig.data = {
+      id: this.id,
+      title: this.newGame.title,
+      description: this.newGame.description,
+      creator: this.newGame.creator,
+      type: this.newGame.type,
+      tags: this.newGame.tags
+    }
     
+    const dialogRef = this.dialog.open(GameDialogBoxComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(data => {
+      this.gameService.create(data).subscribe((newGameFromServer) => {
+        this.newGameEvent.emit(newGameFromServer);
+        console.log(data);
+      });
+    });
   }
 }
